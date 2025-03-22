aeb4af90b35415073d8314a6fd43edd815ec2195
General Jack O'Neill <68293825+StarGate-One@users.noreply.github.com>
2025-03-22 14:00:14 -0400

    Update to version 11.0.4 of fmt

--- vcpkg-tool.cmd
+++ vcpkg-tool.cmd
@@ -29,7 +29,8 @@
     @goto :end-script
 )
 :cont-2
-@set _VCPKG_FMT_URL=https://github.com/fmtlib/fmt/archive/refs/tags/10.2.1.tar.gz
+@rem set _VCPKG_FMT_URL=https://github.com/fmtlib/fmt/archive/refs/tags/10.2.1.tar.gz
+@set _VCPKG_FMT_URL=https://github.com/fmtlib/fmt/archive/refs/tags/11.0.2.tar.gz
 @set _VCPKG_CMAKERC_URL=https://github.com/vector-of-bool/cmrc/archive/refs/tags/2.0.1.tar.gz
 
 @call %_VCPKG_GIT_SCRIPTS_DIR%\date-time.cmd
