73d63416078b2e84ef660b68d8384a281e40addc
General Jack O'Neill <68293825+StarGate-One@users.noreply.github.com>
2026-02-11 01:32:01 -0500

    Another change due to vcpkg-tool update

--- vcpkg-version.cmd
+++ vcpkg-version.cmd
@@ -1,7 +1,7 @@
 @echo off 
 @rem parse the VCPKG VERSION output
 set _vcpkg_version=EMPTY
-for /f "tokens=6*" %%g in ('.\vcpkg.exe version') do (
+for /f "tokens=6*" %%g in ('%_VCPKG_TOOL_EXE% version') do (
    set _vcpkg_version=%%g
    if %_vcpkg_version% NEQ "EMPTY" (
       goto next
